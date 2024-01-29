351f5f1a718a78c997d953407c7863fb5cf3a010
wlissis <wlissisdev@gmail.com>
2024-01-29 16:11:44 -0300

    add style

---
+++ src/components/answer/index.jsx
@@ -0,0 +1,10 @@
+import styles from './style.module.css';
+
+export function Answer(){
+    return(
+        <div className={styles.answer}>
+            <span>A</span>
+            <p>Answer</p>
+        </div>
+    )
+}
--- src/pages/quiz/index.jsx
+++ src/pages/quiz/index.jsx
@@ -1,12 +1,28 @@
-import { Link,useSearchParams } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
+import { Answer } from "../../components/answer";
 
-export function Quiz(){
+import styles from './style.module.css';
+
+
+export function Quiz() {
     const [searchParams] = useSearchParams();
     const category = searchParams.get('category');
-    return(
-        <div>
-            <h1>Quiz {category}</h1>
-            <Link to={'/'}>Go Home!</Link>
+    return (
+        <div className={styles.container}>
+            <div className={styles.question}>
+                <div>
+                    <span>Questão 1 de 10</span>
+                    <p>Pergunta sobre qualquer coisa</p>
+                </div>
+                <span className={styles.progress}><div></div></span>
+
+            </div>
+            <div className={styles.answers}>
+                <Answer/>
+                <Answer/>
+                <Answer/>
+                <Answer/>
+            </div>
 
         </div>
     )
